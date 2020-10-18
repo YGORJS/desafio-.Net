5b9716746ea70902bac6cac49542fa5140be2178
YGOR JOHASSON SILVA <ygorjs26@gmail.com>
2020-10-18 17:08:47 -0300

    Desafio - Anotações do fone
    Adicionando regras de validação

--- YgorTeste/YgorTeste/Models/Fone.cs
+++ YgorTeste/YgorTeste/Models/Fone.cs
@@ -14,10 +14,13 @@ namespace YgorTeste.Models
         [Key, Column(Order = 0)]
         public int Id { get; set; }
 
+        [StringLength(9, ErrorMessage = "Invalid fields", MinimumLength = 9)]
         public string numero { get; set; }
 
+        [StringLength(2, ErrorMessage = "Invalid fields", MinimumLength = 2)]
         public string Codigoarea { get; set; }
 
+        [StringLength(3, ErrorMessage = "Invalid fields", MinimumLength = 3)]
         public string CodigoPais { get; set; }
 
         public int usuarioid   { get; set; }
