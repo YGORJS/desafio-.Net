8cdd98a103f9457ce7d100499dc5d93bc87135d7
YGOR JOHASSON SILVA <ygorjs26@gmail.com>
2020-10-18 14:48:10 -0300

    Desafio - Testes XUNIT.
    Testes realizados sobre o metodo de conferencia para o email. Feito apenas para esse método para demonstração dos conceitos e praticas.

---
+++ YgorTeste/XUnitTestYgor/UsuarioDALServiceTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using YgorTeste.DAL;
+using YgorTeste.Models;
+
+namespace XUnitTestYgor
+{
+    public class UsuarioDALServiceTest : IUsuarioDAL
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        bool IUsuarioDAL.AtualizarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Usuario> IUsuarioDAL.EmailExiste(string email)
+        {
+            List<Usuario> usuarios  = new List<Usuario>();
+            Usuario usu = new Usuario();           
+
+            usu.email = "hello@world.com";
+            usu.firstName = "Hello";
+            usu.lastName = "Word";
+            usu.password = "hunter";
+
+            usuarios.Add(usu);
+
+            return usuarios;
+        }
+
+
+
+        Usuario IUsuarioDAL.ObterUsuario(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IUsuarioDAL.UsuarioExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YgorTeste/XUnitTestYgor/UsuarioDALTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using YgorTeste.Controllers;
+using YgorTeste.DAL;
+using YgorTeste.Models;
+
+namespace XUnitTestYgor
+{
+    public class UsuarioDALTest
+    {
+        IUsuarioDAL _usuarioDal;
+
+
+        public UsuarioDALTest() 
+        {
+            _usuarioDal = new UsuarioDALServiceTest();
+        }
+
+
+        [Fact]
+        public void EmailExiste_UsuarioComEmail_ReturnaUsuario()
+        {
+            // Act 
+            var okResult = _usuarioDal.EmailExiste("hello@world.com");
+            // Assert
+            Assert.IsType<List<Usuario>>(okResult);
+        }
+
+        [Fact]
+        public void EmailExiste_EmailUnico_ApenasUm()
+        {
+            // Act
+            var okResult = _usuarioDal.EmailExiste("hello@world.com");
+
+            //Descomentar para favorecer o teste com capitura de erro
+            //Usuario usu = new Usuario();
+            //usu.email = "hello@world.com";
+            //usu.firstName = "Hello";
+            //usu.lastName = "Word";
+            //usu.password = "hunter";
+
+            //okResult.Add(usu);
+
+            // Assert
+            var items = Assert.IsType<List<Usuario>>(okResult);
+            Assert.Single(items);
+        }
+
+    }
+}
