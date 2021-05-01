d9aa5039cc168aa712b45303ca267cb8031a2ade
YGOR JOHASSON SILVA <ygorjs26@gmail.com>
2021-05-01 10:16:39 -0300

    teste commit

--- YgorTeste/YgorTeste/Controllers/UsuarioController.cs
+++ YgorTeste/YgorTeste/Controllers/UsuarioController.cs
@@ -116,6 +116,7 @@ namespace YgorTeste.Controllers
                     });
                 }
 
+
                 _usuarioBLL.CadastrarUsuario(usuario, usuario.fone);
                              
 
